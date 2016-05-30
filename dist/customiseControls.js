aaa446bcd6c5db669b3b4dcd23705b32edb01743
Andreas Wildi <Largo@users.noreply.github.com>
2016-05-30 17:43:15 +0200

    Fix that object jumps when a rotation is started
    Objects jumped if they had different origins when a rotation was started

--- dist/customiseControls.js
+++ dist/customiseControls.js
@@ -597,21 +597,32 @@
             this.renderAll();
         },
 
-        _setOriginToCenter: function( target ) {
-            target._originalOriginX = target.originX;
-            target._originalOriginY = target.originY;
-
-            target.set( {
-                originX: 'center',
-                originY: 'center'
-            } );
+      _setOriginToCenter: function( target ) {
+          target._originalOriginX = target.originX;
+          target._originalOriginY = target.originY;
+    
+          var center = target.getCenterPoint();
+    
+          target.set({
+            originX: 'center',
+            originY: 'center',
+            left: center.x,
+            top: center.y
+          });
         },
-
+    
         _setCenterToOrigin: function( target ) {
-            target.set( {
-                originX: target._originalOriginX,
-                originY: target._originalOriginY
-            } );
+          var originPoint = target.translateToOriginPoint(
+          target.getCenterPoint(),
+          target._originalOriginX,
+          target._originalOriginY);
+    
+          target.set({
+              originX: target._originalOriginX,
+              originY: target._originalOriginY,
+              left: originPoint.x,
+              top: originPoint.y
+          });
         },
 
         /**
